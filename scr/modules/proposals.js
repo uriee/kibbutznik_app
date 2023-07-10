c0861690884c41b731497588b92a2f842fa15b5c
Uri EvenEzra <uriee@Uris-MBP.home>
2023-07-10 22:38:45 +0300

    walking twords a pulse. work on proposal and pulse modules

--- scr/modules/proposals.js
+++ scr/modules/proposals.js
@@ -1,8 +1,23 @@
 // models/Proposals.js
 const createAstraClient = require('../path_to_your_file');
 
+
+const PROPOSAL_STATUS_ENUM = ['Draft', 'OutThere', 'Assigned', 'Canceled', 'OnTheAir', 'Accepted', 'Rejected'];
+const PROPOSAL_TYPE_ENUM = ['Membership', 'ThrowOut', 'AddStatement', 'RemoveStatement', 'replaceStatement', 'ChangeVariable', 'AddAction', 'EndAction', 'JoinAction', 'Funding'];
+const PROPOSAL_STATUS_LIFECYCLE = ['Draft', 'OutThere', 'Assigned', 'Accepted'];
+
 class Proposals {
+
+    
     static async create(proposal) {
+        if (!PROPOSAL_STATUS_ENUM.includes(proposal.proposal_status)) {
+            throw new Error(`Invalid proposal_status: ${proposal.proposal_status}`);
+        }
+        
+        if (!PROPOSAL_TYPE_ENUM.includes(proposal.proposal_type)) {
+            throw new Error(`Invalid proposal_type: ${proposal.proposal_type}`);
+        }
+
         const astraClient = await createAstraClient();
         const query = 'INSERT INTO Proposals (community_id, proposal_id, proposal_text, proposal_status, proposal_type, proposal_support) VALUES (?, ?, ?, ?, ?, ?)';
         const params = [proposal.community_id, proposal.proposal_id, proposal.proposal_text, proposal.proposal_status, proposal.proposal_type, proposal.proposal_support];
@@ -59,6 +74,56 @@ class Proposals {
         const result = await astraClient.execute(query, params);
         return result.rows;
     }
+
+
+    static async UpdateStatus(proposalId, direction) {
+        const astraClient = await createAstraClient();
+        
+    
+        const proposal = await this.findById(proposalId);
+        if (!proposal || proposal.length === 0) {
+            throw new Error(`Proposal with id ${proposalId} not found`);
+        }
+
+        const currentIndex = PROPOSAL_STATUS_LIFECYCLE.indexOf(proposal[0].proposal_status);
+
+        if (currentIndex === -1 || currentIndex === PROPOSAL_STATUS_LIFECYCLE.length - 1) {
+            throw new Error(`Cannot update proposal with status ${proposal[0].proposal_status}`);
+        }
+
+        let newStatus;
+        if (direction) {
+            newStatus = PROPOSAL_STATUS_LIFECYCLE[currentIndex + 1];
+        } else {
+            newStatus = currentIndex === 2 ? 'Rejected' : 'Canceled';
+        }
+
+        const query = 'UPDATE Proposals SET proposal_status = ? WHERE proposal_id = ?';
+        const params = [newStatus, proposalId];
+        await astraClient.execute(query, params);
+    }
+
+    static async AssignedToPulse(proposalId) {
+        const astraClient = await createAstraClient();
+
+        const proposal = await this.findById(proposalId);
+        if (!proposal || proposal.length === 0) {
+            throw new Error(`Proposal with id ${proposalId} not found`);
+        }
+
+
+        const activePulse = await Pulses.findActive();  
+        if (!activePulse || activePulse.length === 0) {
+            throw new Error(`No active pulse found`);
+        }
+
+        const query = 'UPDATE Proposals SET pulse_id = ? WHERE proposal_id = ?';
+        const params = [activePulse[0].pulse_id, proposalId];
+        await astraClient.execute(query, params);
+
+        await this.UpdateStatus(proposalId, true);
+    }
+
 }
 
 module.exports = Proposals;
--- scr/modules/pulses.js
+++ scr/modules/pulses.js
@@ -1,6 +1,8 @@
 // models/Pulses.js
 const createAstraClient = require('../path_to_your_file');
 
+const PULSE_STATUS_LIFECYCLE = ['Next', 'Active', 'Done'];
+
 class Pulses {
     static async findByCommunityId(communityId) {
         const astraClient = await createAstraClient();
@@ -28,8 +30,30 @@ class Pulses {
     static async create(pulse) {
         const astraClient = await createAstraClient();
         // assuming `pulse` is an object with fields: community_id, pulse_id, pulse_timestamp
-        const query = 'INSERT INTO Pulses (community_id, pulse_id, pulse_timestamp) VALUES (?, ?, ?)';
-        const params = [pulse.community_id, pulse.pulse_id, pulse.pulse_timestamp];
+        const query = 'INSERT INTO Pulses (community_id, pulse_id, pulse_timestamp, pulse_status) VALUES (?, ?, ?)';
+        const params = [pulse.community_id, pulse.pulse_id, pulse.pulse_timestamp, 'Next'];
+        await astraClient.execute(query, params);
+    }
+
+    static async findActive() {
+        const astraClient = await createAstraClient();
+        const query = 'SELECT * FROM Pulses WHERE pulse_status = ?';
+        const params = ['Active'];
+        const result = await astraClient.execute(query, params);
+        return result.rows;
+    }
+
+    static async IncrementStatus(pulse) {
+        const astraClient = await createAstraClient();
+
+        const currentIndex = PULSE_STATUS_LIFECYCLE.indexOf(pulse.pulse_status);
+        if (currentIndex === -1 || currentIndex === PULSE_STATUS_LIFECYCLE.length - 1) {
+            throw new Error(`Cannot increment status for pulse with status ${pulse.pulse_status}`);
+        }
+
+        const newStatus = PULSE_STATUS_LIFECYCLE[currentIndex + 1];
+        const query = 'UPDATE Pulses SET pulse_status = ? WHERE pulse_id = ?';
+        const params = [newStatus, pulse.pulse_id];
         await astraClient.execute(query, params);
     }
 
